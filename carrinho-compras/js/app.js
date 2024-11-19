e59376f5dee83be2dd5bc0d03c8bd095fa9f0f2f
Lais-RDS <laisraf_fnds@hotmail.com>
2024-11-19 16:01:37 -0300

    terminando funçoes do carrinho

--- carrinho-compras/js/app.js
+++ carrinho-compras/js/app.js
@@ -1,3 +1,10 @@
+let totalGeral = 0; 
+document.getElementById('lista-produtos').innerHTML = '';
+document.getElementById('valor-total').textContent = 'R$ 0';
+document.getElementById('quantidade').value = 1;
+
+
+
 function adicionar(){
 let produto = document.getElementById('produto').value;
 let nomeProduto = produto.split('-')[0];
@@ -8,11 +15,21 @@ let carrinho = document.getElementById('lista-produtos');
 carrinho.innerHTML +=  `<section class="carrinho__produtos__produto">
     <span class="texto-azul">${quantidade}x</span> ${nomeProduto} <span class="texto-azul">${preco}</span>
 </section>`;
+totalGeral += preco;
+let CampoTotal = document.getElementById('valor-total');
+CampoTotal.textContent = `R$ ${totalGeral}`;
+document.getElementById('quantidade').value = 1;
+
 
 }
 
 
+
 function limpar(){
 
+    totalGeral = 0; 
+    document.getElementById('lista-produtos').innerHTML = '';
+    document.getElementById('valor-total').textContent = 'R$ 0';
+    document.getElementById('quantidade').value = 1;
 }
 
