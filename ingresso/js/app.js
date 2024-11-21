b33ba602069b2802e7fa8da7597c81371e1c4f0b
Lais-RDS <laisraf_fnds@hotmail.com>
2024-11-20 23:22:39 -0300

    adicionando funçoes de verificar e comprar ingressos

---
+++ ingresso/js/app.js
@@ -0,0 +1,48 @@
+
+
+function comprar(){
+    let tipo = document.getElementById('tipo-ingresso')
+    let qtd = parseInt(document.getElementById('qtd').value);
+    
+    if (tipo.value == 'pista'){
+        comprarPista(qtd);
+
+    } else if (tipo.value == 'superior'){
+        comprarSuperior(qtd);
+    } else {
+        comprarInferior(qtd);
+        }
+
+}
+
+function comprarPista(qtd){
+    let qtdPista = parseInt(document.getElementById('qtd-pista').textContent);
+    if (qtd > qtdPista) {
+        alert ('Quantidade indisponivel para pista');
+    }else {
+        qtdPista = qtdPista - qtd;
+        document.getElementById('qtd-pista').textContent = qtdPista;
+        alert ('Compra Realizada com sucesso!');
+    }
+}
+
+function comprarSuperior(qtd){
+    let qtdsuperior = parseInt(document.getElementById('qtd-superior').textContent);
+    if (qtd > qtdsuperior) {
+        alert ('Quantidade indisponivel para superior');
+    }else {
+        qtdsuperior = qtdsuperior - qtd;
+        document.getElementById('qtd-superior').textContent = qtdsuperior;
+        alert ('Compra Realizada com sucesso!');
+    }
+}
+function comprarInferior(qtd){
+    let qtdinferior = parseInt(document.getElementById('qtd-inferior').textContent);
+    if (qtd > qtdinferior) {
+        alert ('Quantidade indisponivel para inferior');
+    }else {
+        qtdinferior = qtdinferior - qtd;
+        document.getElementById('qtd-inferior').textContent = qtdinferior;
+        alert ('Compra Realizada com sucesso!');
+    }
+}
